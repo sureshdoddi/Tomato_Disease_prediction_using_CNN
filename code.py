34290125f523b01c41ad1f855c8342a034bec19b
sureshdoddi <60309253+sureshdoddi@users.noreply.github.com>
2021-05-19 13:05:59 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,4 @@
-
+# Early Detection of Tomato Leaf Detection is a major necessity in a growing Tomato plants
+#It is necessary that leaf diseases in plants are detected at a very early stage and predictive mechanisms to be adopted to make them safe and avoid losses to the agri-based economy.
+#This code proposes to identify the Tomato Plant Leaf disease using image processing techniques based on Image segmentation, clustering, and open-source algorithms.
+# Thus all contributing to a reliable, safe, and accurate system of leaf disease with the specialization to Tomato Plants.
